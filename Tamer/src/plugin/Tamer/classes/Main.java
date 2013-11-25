4819275e6f5881842c9b9962c7d60b509e2eeff6
Zxios <mcplaylab@gmail.com>
2013-11-25 18:24:54 -0500

    Made sure it worked :D

---
+++ Tamer/src/plugin/Tamer/classes/Main.java
@@ -0,0 +1,22 @@
+package plugin.Tamer.classes;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+	public void onEnable() {
+		
+	}
+	
+	public boolean onCommand() {
+		
+		return true;
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
